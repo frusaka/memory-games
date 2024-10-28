3efaf928cde59793b8d7ec4247a1be7d574719f6
frusaka <fredrick.rusaka@gmail.com>
2024-10-28 06:29:12 -0500

    Add flip animations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let prevEmoji;
+let prevPaired = { pending: false, timeoutId: null, timeoutFunc: null };
 let emojis = ["🦥", "🦆", "🐼", "🦋", "🦚", "🐋", "🐦", "🐶"];
 emojis = emojis.concat(emojis);
 
@@ -11,25 +12,34 @@ function shuffle(arr) {
 
 function gameLogic(event) {
   const emojiCard = event.target;
-  emojiCard.classList.add("emoji-revealed");
+  if (prevEmoji === emojiCard) return;
+
+  emojiCard.classList.add("emoji-reveal");
+
+  if (prevPaired.pending) {
+    clearTimeout(prevPaired.timeoutId);
+    prevPaired.timeoutFunc();
+  }
 
   if (!prevEmoji) {
     prevEmoji = emojiCard;
     return;
   }
-  if (prevEmoji === emojiCard) return;
 
   if (emojiCard.innerHTML == prevEmoji.innerHTML) {
     emojiCard.removeEventListener("click", gameLogic);
     prevEmoji.removeEventListener("click", gameLogic);
+    prevEmoji = null;
   } else {
-    const localprevEmoji = prevEmoji;
-    setTimeout(() => {
-      localprevEmoji.classList.remove("emoji-revealed");
-      emojiCard.classList.remove("emoji-revealed");
-    }, 300);
+    prevPaired.pending = true;
+    prevPaired.timeoutFunc = () => {
+      prevEmoji.classList.remove("emoji-reveal");
+      emojiCard.classList.remove("emoji-reveal");
+      prevEmoji = null;
+      prevPaired.pending = false;
+    };
+    prevPaired.timeoutId = setTimeout(prevPaired.timeoutFunc, 700);
   }
-  prevEmoji = null;
 }
 
 function renderCards() {
@@ -42,8 +52,8 @@ function renderCards() {
   document.querySelectorAll(".js-emoji").forEach((emojiCard) => {
     /*
     // Wanna get a full picture before starting?
-    emojiCard.classList.add("emoji-revealed");
-    setTimeout(()=>emojiCard.classList.remove("emoji-revealed"),1000)
+    emojiCard.classList.add("emoji-reveal");
+    setTimeout(()=>emojiCard.classList.remove("emoji-reveal"),1000)
     */
     emojiCard.addEventListener("click", gameLogic);
   });
