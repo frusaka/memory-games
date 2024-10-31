b832cd21887ea87a74b3b8194e5bf2d903d78415
frusaka <fredrick.rusaka@gmail.com>
2024-10-30 22:01:20 -0500

    Initiate scoring system and imporve styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let prevEmoji;
+let prevEmoji, sizeTimoutId;
 let emojis = [
   "🐊",
   "🦆",
@@ -33,15 +33,19 @@ let emojis = [
   "🦒",
   "🦇",
   "🐑",
+  "🐞",
+  "🐥",
+  "🐒",
+  "🦓",
+  "🐃",
+  "🦛",
+  "🦘",
 ];
-let pairedEmojis = 0;
-let grid = {
-  rows: 4,
-  columns: 4,
-  total() {
-    return Math.floor((this.rows * this.columns) / 2);
-  },
-};
+
+let gridTotal = 8;
+let pairedEmojis = (totalClicks = wrongClicks = 0);
+let bestScore =
+  JSON.parse(localStorage.getItem("image-pairing-best-score")) || 0;
 let prevPaired = {
   pending: false,
   timeoutId: null,
@@ -54,13 +58,19 @@ let prevPaired = {
   },
 };
 
-const cardsGrid = document.querySelector(".js-cards");
+const cardsGrid = document.getElementById("cards");
 
 function shuffle(arr) {
   return arr
     .map((value) => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value);
+}
+
+function cardsArray(total) {
+  let gameEmojis = shuffle(emojis).slice(0, total);
+  gameEmojis = gameEmojis.concat(gameEmojis);
+  return shuffle(gameEmojis);
 }
 
 function celebrate(currEmoji, success) {
@@ -80,6 +90,7 @@ function celebrate(currEmoji, success) {
     if (!success) {
       prevEmoji.classList.remove("emoji-reveal");
       currEmoji.classList.remove("emoji-reveal");
+      wrongClicks++;
     }
 
     prevEmoji = null;
@@ -88,6 +99,7 @@ function celebrate(currEmoji, success) {
 
   prevPaired.timeoutId = setTimeout(prevPaired.timeoutFunc, 700);
   prevPaired.pending = true;
+  totalClicks++;
 }
 
 function gameLogic(event) {
@@ -117,7 +129,7 @@ function gameLogic(event) {
 
   // Finished the game?
   pairedEmojis++;
-  if (pairedEmojis < grid.total()) celebrate(currEmoji, true);
+  if (pairedEmojis < gridTotal) celebrate(currEmoji, true);
   else resetGame();
 }
 
@@ -131,7 +143,12 @@ function resetGame() {
     });
     setTimeout(renderCards, 500);
   }, 1000);
-
+  // Record Score
+  let newScore = Math.round(((totalClicks - wrongClicks) / totalClicks) * 100);
+  if (newScore > bestScore) {
+    bestScore = newScore;
+    localStorage.setItem("image-pairing-best-score", bestScore);
+  }
   // Reset previous pointers
   pairedEmojis = 0;
   prevEmoji = null;
@@ -139,38 +156,48 @@ function resetGame() {
 
 function renderCards() {
   let cardsHTML = "";
-  let gameEmojis = shuffle(emojis).slice(0, grid.total());
-  gameEmojis = gameEmojis.concat(gameEmojis);
-  // Fill empty spot on a grid with an odd number of cards
-  if ((grid.rows * grid.columns) % 2) {
-    gameEmojis.push("");
-  }
-  //Generate html for the cards
-  shuffle(gameEmojis).forEach((emoji) => {
-    cardsHTML += `<div class="emoji emoji-hide js-emoji">${emoji}</div>`;
+  cardsArray(gridTotal).forEach((emoji) => {
+    cardsHTML += `<div class="emoji js-emoji">${emoji}</div>`;
   });
   // Fill grid
-  cardsGrid.style = `
-    --cols:${grid.columns};
-    --rows:${grid.rows};
-    grid-template-columns:repeat(${grid.rows},1fr);
-  `;
+  updateGrid();
   cardsGrid.innerHTML = cardsHTML;
+  document.getElementById("accuracy").innerText = `${bestScore}%`;
   document
     .querySelectorAll(".js-emoji")
     .forEach((emoji) => emoji.addEventListener("click", gameLogic));
 }
 
-document.querySelector(".js-start").onclick = () => {
-  document.querySelector(".js-home").style.visibility = "collapse";
-  document.querySelector(".js-game").style.display = "flex";
-};
+function updateGrid() {
+  // let totalArea = (Math.min(window.innerWidth, window.innerHeight) * 0.8) ** 2;
+  let totalArea = window.innerWidth * window.innerHeight ** 0.854;
+  let size = Math.floor(Math.sqrt(totalArea / (gridTotal * 2)));
+  cardsGrid.style.setProperty("--size", `${size}px`);
+}
 
-document.querySelectorAll("input[type='range']").forEach((input) => {
-  input.oninput = () => {
-    grid[input.id] = +input.value;
-    renderCards();
-  };
+window.addEventListener("resize", updateGrid);
+
+document.getElementById("start").addEventListener("click", () => {
+  document.getElementById("home").style.visibility = "collapse";
+  document.getElementById("game").style.display = "flex";
+});
+
+document.getElementById("add").addEventListener("click", () => {
+  if (gridTotal == emojis.length) {
+    return;
+  }
+  gridTotal++;
+  document.getElementById("size").innerHTML = gridTotal;
+  renderCards();
+});
+
+document.getElementById("sub").addEventListener("click", () => {
+  if (gridTotal == 2) {
+    return;
+  }
+  gridTotal--;
+  document.getElementById("size").innerHTML = gridTotal;
+  renderCards();
 });
 
 renderCards();
